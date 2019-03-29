ecae62d7c703ccb75b239934c591f5be9f9b03f1
wiktorlesiak <B00087701@student.itb.ie>
2019-03-29 12:27:01 +0000

    adding lanes

--- lanedetection.py
+++ lanedetection.py
@@ -3,26 +3,41 @@ import cv2 as cv
 import numpy as np
 
 
-cap = cv2.VideoCapture('StayingInLane.avi')
-# canny edge detection
+video = cv2.VideoCapture('StayingInLane.avi')
+fgbg = cv2.createBackgroundSubtractorMOG2()
 while (1):
+    ret, orig_frame = video.read()
+    if not ret:
+        video = cv2.VideoCapture('StayingInLane.avi')
+        continue
+    fgmask = fgbg.apply(orig_frame)
 
-    _, frame = cap.read()
+    frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
+    # grayscale video
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    lower_red = np.array([30, 150, 50])
-    upper_red = np.array([255, 255, 180])
-
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    # hsv mask
+    lower = np.array([0, 0, 212])
+    upper = np.array([131, 255, 255])
+    mask = cv2.inRange(hsv, lower, upper)
     res = cv2.bitwise_and(frame, frame, mask=mask)
+    # canny edge detection
+    edges = cv2.Canny(frame, 120, 200)
+    # Hough Lines
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=50)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    # output video
+    cv2.imshow("edges", edges)
+    cv2.imshow('lines', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('Original', fgmask)
 
-    cv2.imshow('Original', frame)
-    edges = cv2.Canny(frame, 100, 200)
-    cv2.imshow('Edges', edges)
 
-    k = cv2.waitKey(5) & 0xFF
+    k = cv2.waitKey(20) & 0xFF
     if k == 27:
         break
 
 cv2.destroyAllWindows()
-cap.release()
+video.release()
